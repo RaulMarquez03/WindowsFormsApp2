dc011a3dc2acad063eab3cea33e1684b30d5c978
raulmarquez <raulmarqueroig15@gmail.com>
2023-01-27 13:18:08 +0100

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class btClickAqui
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbHelloWord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cick Aqui";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbHelloWord
+            // 
+            this.lbHelloWord.AutoSize = true;
+            this.lbHelloWord.Location = new System.Drawing.Point(163, 252);
+            this.lbHelloWord.Name = "lbHelloWord";
+            this.lbHelloWord.Size = new System.Drawing.Size(0, 16);
+            this.lbHelloWord.TabIndex = 1;
+            this.lbHelloWord.Click += new System.EventHandler(this.lbHelloWord_Click);
+            // 
+            // btClickAqui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(437, 387);
+            this.Controls.Add(this.lbHelloWord);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "btClickAqui";
+            this.Text = "RaulMarquezRoig";
+            this.Load += new System.EventHandler(this.btClickAqui_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbHelloWord;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class btClickAqui : Form
+    {
+        public btClickAqui()
+        {
+            InitializeComponent();
+        }
+
+        private void lbHelloWord_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbHelloWord.Text = "Hello World";
+        }
+
+        private void btClickAqui_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
